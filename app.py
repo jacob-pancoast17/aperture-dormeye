c661a5e7a802be50a89e35e3577df0af1dea24d6
Jacob P <jacob.pancoast17@gmail.com>
2025-03-18 21:26:32 -0400

    Modify index.html to show a basic login page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import *
 
 app = Flask(__name__)
+
+#@app.route("/login", methods=['POST'])
+#def login():
+#    print("Login")
+#    return "Pressed"
 	
 @app.route('/', methods=['GET'])
 def default():
